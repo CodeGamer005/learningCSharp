5d6a1c9d469a7b80efb515898db5a4dcb3d8ec30
CodeGamer005 <134540938+CodeGamer005@users.noreply.github.com>
2023-05-24 23:19:42 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace csharp_tutorial {
+    class games 
+    {
+        public string name;
+        public int yearReleased;
+
+        public void print()
+        {
+            Console.WriteLine(name + " is a great game");
+            Console.WriteLine("it was released in the year " + yearReleased);
+        }
+    }
+
+    class Program{
+
+        static void Main(String[] args)
+        {
+            Console.Title = "Skynet";  
+            Console.WindowHeight = 40;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            games game01 = new games();
+            game01.name = "breath of the wild";
+            game01.yearReleased = 2017;
+
+            game01.print();
+            Console.ReadKey();
+            
+
+        }
+    }
+}
